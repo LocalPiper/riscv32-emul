2832fa88f6733c0c29a0f4f7f2688730335890af
Local Piper <artsor53@gmail.com>
2025-08-22 18:49:43 +0300

    feat: type-i commands

--- src/instructions.cpp
+++ src/instructions.cpp
@@ -43,6 +43,28 @@ void execute_i_type(uint32_t inst, uint32_t regs[32]) {
   const uint8_t funct3 = get_funct3(inst);
   const int32_t imm = get_imm_i(inst);
 
-  if (funct3 == 0x0) // addi rd, rs1, imm
+  const uint8_t shamt = imm & 0x1F; // imm[4:0]
+  const uint8_t funct7 = (imm >> 5) & 0x7F; // imm[11:5]
+
+  if (funct3 == 0x0) { // addi rd, rs1, imm
     regs[rd] = regs[rs1] + imm;
+  } else if (funct3 == 0x4) { // xori rd, rs1, imm
+    regs[rd] = regs[rs1] ^ imm;
+  } else if (funct3 == 0x6) { // ori rd, rs1, imm
+    regs[rd] = regs[rs1] | imm;
+  } else if (funct3 == 0x7) { // andi rd, rs1, imm
+    regs[rd] = regs[rs1] & imm;
+  } else if (funct3 == 0x1 && funct7 == 0x00) { // slli rd, rs1, imm
+    regs[rd] = regs[rs1] << shamt;
+  } else if (funct3 == 0x5) {
+    if (funct7 == 0x00) { // srli rd, rs1, imm
+      regs[rd] = regs[rs1] >> shamt;
+    } else if (funct7 == 0x20) { // srai rd, rs1, imm
+      regs[rd] = static_cast<uint32_t>(static_cast<int32_t>(regs[rs1]) >> shamt);
+    }
+  } else if (funct3 == 0x2) { // slti rd, rs1, imm
+    regs[rd] = (static_cast<int32_t>(regs[rs1]) < imm)? 1 : 0;
+  } else if (funct3 == 0x3) { // sltiu rd, rs1, imm
+    regs[rd] = (regs[rs1] < static_cast<uint32_t>(imm))? 1 : 0;
+  }
 }
